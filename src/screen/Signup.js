672c8060e448a0a5d2b7377f1588a7c71eeb4cee
NaumanRecat <naumantariq2977@gmail.com>
2020-12-31 15:18:53 +0500

    base 2

---
+++ src/screen/Signup.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View} from 'react-native';
+
+export class Signup extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'green',
+        }}></View>
+    );
+  }
+}
